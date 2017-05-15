36c29b1eb6492a1e0db0b80940b719c99c6ecde5
vincent <leizhang>
2017-05-15 18:02:46 +0800

    errorbage

--- src/main/java/com/gi/xm/es/config/ServerStatusProperties.java
+++ src/main/java/com/gi/xm/es/config/ServerStatusProperties.java
@@ -2,8 +2,8 @@ package com.gi.xm.es.config;
 
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.ErrorPage;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 
@@ -13,13 +13,12 @@ import org.springframework.http.HttpStatus;
 @ConfigurationProperties
 public class ServerStatusProperties {
 
-    static ErrorPage ernror404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error/404");
     @Bean
     public EmbeddedServletContainerCustomizer containerCustomizer(){
         return new EmbeddedServletContainerCustomizer(){
             @Override
             public void customize(ConfigurableEmbeddedServletContainer container) {
-                container.addErrorPages(ernror404);
+                container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/error/404"));
                 container.addErrorPages(new ErrorPage(HttpStatus.BAD_REQUEST, "/error/400"));
                 container.addErrorPages(new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error/500"));
                 container.addErrorPages(new ErrorPage(Throwable.class,"/error/500"));
