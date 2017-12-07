d27441ad43f4cbb605af5ed2ed868d7b2bc9ee43
chunhuanzhang <chunhuanzhang@galaxyinternet.com>
2017-12-07 10:26:33 +0800

    加文档注释

--- src/main/java/com/gi/xm/es/main/SearchApp.java
+++ src/main/java/com/gi/xm/es/main/SearchApp.java
@@ -1,15 +1,18 @@
 package com.gi.xm.es.main;
 
+import com.gi.xm.es.config.SwaggerConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @EnableEurekaClient
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.gi.xm.es"})
+@Import(SwaggerConfiguration.class)
 public class SearchApp {
     private static final Logger LOG = LoggerFactory.getLogger(SearchApp.class);
 
